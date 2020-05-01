d64eabc3c341d6bb4af752721e6f4ca0e6afad85
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-05-01 16:34:50 -0300

    Refatorando o código

--- scrip.js
+++ scrip.js
@@ -1,44 +1,47 @@
 console.log("Javascript carregado");
 
 
-function validaCPF(cpf){
+function verificarDigito(cpf, posicaoDigitoVerificador) {
+    var aux = 0;
 
-    if(cpf.length != 11){
-        return false;
+    if (posicaoDigitoVerificador == 0) {
+        aux = 9
+    } else if (posicaoDigitoVerificador == 1) {
+        aux = 10
     }
-
-    var numeros = cpf.substring(0,9);
+    var numeros = cpf.substring(0, aux);
     var digitos = cpf.substring(9);
-
     var soma = 0;
 
-    for(var i = 10; i > 1; i--){
-        soma += numeros.charAt(10 - i) * i;
+    for (var i = (aux + 1); i > 1; i--) {
+        soma += numeros.charAt((aux + 1) - i) * i;
     }
 
     var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
 
-    if(resultado != digitos.charAt(0)){
+    if (resultado != digitos.charAt(posicaoDigitoVerificador)) {
         return false
     }
 
-    soma  = 0;
-    numeros = cpf.substring(0, 10);
-
-    for(var k = 11; k > 1; k--){
-        soma += numeros.charAt(11 - k) * k;
-    }
-
-    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-
-    if(resultado != digitos.charAt(1)){
-        return false;
-    }
-
-    return true;
+    return true
 }
 
-function validacao(){
+function validaCPF(cpf) {
+
+    if (cpf.length == 11) {
+
+        var primeiroDigitoValido = verificarDigito(cpf, 0);
+        var segundoDigitoValido = verificarDigito(cpf, 1);
+
+        if (primeiroDigitoValido && segundoDigitoValido) {
+            return true
+        }
+    }
+
+    return false;
+}
+
+function validacao() {
     console.log("Iniciando validação CPF");
     var cpf = document.getElementById('cpf_digitado').value;
     var resultadoValidacao = validaCPF(cpf);
@@ -46,9 +49,9 @@ function validacao(){
     document.getElementById('error').style.display = 'none';
     document.getElementById('success').style.display = 'none';
 
-    if(resultadoValidacao){
+    if (resultadoValidacao) {
         document.getElementById('success').style.display = 'block';
-    }else{
+    } else {
         document.getElementById('error').style.display = 'block';
     }
 }
