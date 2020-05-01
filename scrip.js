5cbd6056707a46fb201e2d41d62f3c1f53d56dd9
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-05-01 15:59:58 -0300

    Validação do primeiro dígito verificador

--- scrip.js
+++ scrip.js
@@ -2,7 +2,24 @@ console.log("Javascript carregado");
 
 
 function validaCPF(cpf){
-    return false
+
+    if(cpf.length != 11){
+        return false;
+    }
+
+    var numeros = cpf.substring(0,9);
+    var digitos = cpf.substring(9);
+
+    var soma = 0;
+    for(var i = 10; i > 1; i--){
+        soma += numeros.charAt(10 - i) * i;
+    }
+
+    var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+    if(resultado != digitos.charAt(0)){
+        return false
+    }
 }
 
 function validacao(){
