60ed5b8aebb7fd0b22831679a6de5db209c247fa
lidy_gb <lidianegomesbarbosa@gmail.com>
2020-05-01 15:23:02 -0300

    Capturando elementos do html

--- scrip.js
+++ scrip.js
@@ -1,6 +1,20 @@
 console.log("Javascript carregado");
 
+
+function validaCPF(cpf){
+    return false
+}
+
 function validacao(){
     console.log("Iniciando validação CPF");
+    var cpf = document.getElementById('cpf_digitado').value;
+
+    var resultadoValidacao = validaCPF(cpf);
+
+    if(resultadoValidacao){
+        document.getElementById('success').style.display = 'block';
+    }else{
+        document.getElementById('error').style.display = 'block';
+    }
 }
 
